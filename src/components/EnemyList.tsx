3c0cdcb8f02b3296cc42833cc7f37f4a9cc1a169
Snowbits <jericho.jordan.ong@gmail.com>
2025-05-09 16:01:50 +0200

    MCGG minor update
    Added stages array for placeholder
    Added temporary instruction for MCGG list

--- src/components/EnemyList.tsx
+++ src/components/EnemyList.tsx
@@ -1,4 +1,7 @@
 import { useState } from "react";
+
+const stages : string[] =["1-2","1-3","1-4","2-1","2-2","2-4","2-5"]
+
 export default function EnemyList() {
   const [names, setNames] = useState<string[]>(Array(7).fill(""));
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -33,34 +36,17 @@ export default function EnemyList() {
   return (
     <>
       <div className="mcgg">
-        <div className="mcgg-list">
-          {names.map((value, index) => (
-            <div className={"mcgg-list-children"} key={index}>
-              <label htmlFor={`enemy-${index}`} className="center">
-                {getOrdinal(index)} Enemy
-              </label>
-              <input
-                id={"enemy"+index.toString()}
-                key={index}
-                type="text"
-                value={value}
-                onChange={(e) => handleChange(index, e.target.value)}
-                placeholder={`Enemy ${index + 1}`}
-                className="mcgg-list__input"
-              />
-            </div>
-          ))}
-          <div className="mcgg-list__button-container">
-            <button
-              onClick={handleReset}
-              type="submit"
-              className="mcgg-list__button"
-            >
-              Reset
-            </button>
+          <div className="mcgg-instruction">
+            <h2>How to Use MCGG List Helper</h2>
+            <p>{`Enter all of the enemies' names from early stages (1-2 until 2-5) that you fight against in the corresponding input box. Starting from stage 2-6 you will know who you fight against and get much benefits from it  The list is still barely working after someone died.`}</p>
+            <br />
+            <h2>Why should I use MCGG List?</h2>
+            <p>{`Better positioning of the hero to protect the damage dealer or make your CC and burst hero more effective, e.g. Marksman and Assassin heroes should try to avoid CC and burst damage as much as possible.`}</p>
+            <p>{`When playing in a team (especially 4V4), keep your teammates alive when facing each other.`}</p>
+            <br />
+            <p>{`Pro Tip: In desktop browser, after entering the first name, press tab to select the next input box`}</p>
           </div>
-        </div>
-
+        
         <div className="mcgg-status">
           <div className="mcgg-status__container">
             <p>
@@ -94,6 +80,36 @@ export default function EnemyList() {
             </button>
           </div>
         </div>
+        
+        <div className="mcgg-list">
+          {names.map((value, index) => (
+            <div className={"mcgg-list-children"} key={index}>
+              <label htmlFor={`enemy-${index}`} className="center">
+                {getOrdinal(index)} Enemy
+              </label>
+              <input
+                id={"enemy"+index.toString()}
+                key={index}
+                type="text"
+                value={value}
+                onChange={(e) => handleChange(index, e.target.value)}
+                placeholder={`Enemy on ${stages[index]}`}
+                className="mcgg-list__input"
+              />
+            </div>
+          ))}
+          <div className="mcgg-list__button-container">
+            <button
+              onClick={handleReset}
+              type="submit"
+              className="mcgg-list__button"
+            >
+              Reset
+            </button>
+          </div>
+        </div>
+
+        
       </div>
     </>
   );
