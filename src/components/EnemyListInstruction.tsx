b5c4a91e02b72e2b67734100743585632f3ddeb7
Snowbits <jericho.jordan.ong@gmail.com>
2025-05-12 21:35:04 +0200

    Enemy List Instruction Update
    - Adjusted the sizing of instruction for mobile

--- src/components/EnemyListInstruction.tsx
+++ src/components/EnemyListInstruction.tsx
@@ -1,17 +1,17 @@
 const EnemyListInstruction = () => {
   return (
     <>
-      <h2 className="warning">
+      <h2 className="warning mcgg-instruction__h2">
         PLEASE DON'T USE THIS TOOL FOR TOURNAMENT PURPOSE
       </h2>
-      <h2>How to Use MCGG List Helper</h2>
-      <p>{`Enter all of the enemies' names from early stages (1-2 until 2-5) that you fight against in the corresponding input box. Starting from stage 2-6 you will know who you fight against and get much benefits from it  The list is still barely working after someone died.`}</p>
+      <h2 className="mcgg-instruction__h2">How to Use MCGG List Helper</h2>
+      <p className="mcgg-instruction__p">{`Enter all of the enemies' names from early stages (1-2 until 2-5) that you fight against in the corresponding input box. Starting from stage 2-6 you will know who you fight against and get much benefits from it  The list is still barely working after someone died.`}</p>
       <br />
-      <h2>Why should I use MCGG List?</h2>
-      <p>{`Better positioning of the hero to protect the damage dealer or make your CC and burst hero more effective, e.g. Marksman and Assassin heroes should try to avoid CC and burst damage as much as possible.`}</p>
-      <p>{`When playing in a team (especially 4V4), keep your teammates alive when facing each other.`}</p>
+      <h2 className="mcgg-instruction__h2">Why should I use MCGG List?</h2>
+      <p className="mcgg-instruction__p">{`Better positioning of the hero to protect the damage dealer or make your CC and burst hero more effective, e.g. Marksman and Assassin heroes should try to avoid CC and burst damage as much as possible.`}</p>
+      <p className="mcgg-instruction__p">{`When playing in a team (especially 4V4), keep your teammates alive when facing each other.`}</p>
       <br />
-      <p>{`Pro Tip: In desktop browser, after entering the first name, press tab to select the next input box`}</p>
+      <p className="mcgg-instruction__p">{`Pro Tip: In desktop browser, after entering the first name, press tab to select the next input box`}</p>
     </>
   );
 };
