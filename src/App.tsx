b7b476c6386f49eecff156a28d77736d2bd02196
Snowbits <jericho.jordan.ong@gmail.com>
2025-05-13 13:09:47 +0200

    Added Footer & Spell timer instruction
    Added footer component for copyright
    Added instruction for MLBB spell timer

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import EnemyList from "./components/EnemyList";
 import SpellTimer from "./components/SpellTimer";
-import "./App.css";
 import TopBar from "./components/TopBar";
+import Footer from "./components/Footer";
 
 function App() {
   const [currentSite, setCurrentSite] = useState<string>("MCGG");
@@ -12,6 +12,7 @@ function App() {
       <TopBar currentSite={currentSite} setCurrentSite={setCurrentSite}  />
       {currentSite === "MCGG" ? <EnemyList /> : null}
       {currentSite === "MLBB" ? <SpellTimer /> : null}
+      <Footer/>
     </>
   );
 }
---
+++ src/components/SpellTimerInstruction.tsx
@@ -0,0 +1,16 @@
+const SpellTimerInstruction = () => {
+  return (
+    <div className="mlbb-instruction">
+      <h2 className="warning mlbb-instruction__h2 center">
+        PLEASE DON'T USE THIS TOOL FOR TOURNAMENT PURPOSE
+      </h2>
+      <h2 className="mlbb-instruction__h2">How to Use MLBB Spell Timer</h2>
+      <p className="mlbb-instruction__p">{`While drafting, enter the names of all the enemy heroes you will be fighting against in the corresponding select box and predict their roles. Then, while waiting for the match to load, check all of the roles and spells to ensure they are correct.`}</p>
+      <br />
+      <h2 className="mlbb-instruction__h2">Why should I use MLBB Spell Timer?</h2>
+      <p className="mlbb-instruction__p">{`Having a better knowledge of the enemies' spell cooldowns results in more successful ganking and war, as well as better decision-making.`}</p>
+    </div>
+  )
+}
+
+export default SpellTimerInstruction
